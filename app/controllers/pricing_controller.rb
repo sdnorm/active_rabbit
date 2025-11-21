9c1f5d73e6e3852b514bbd8449aa7c9ea21ac523
Alex Shapalov <shapalov@gmail.com>
2025-11-21 11:42:02 -0800

    Implement usage tracking and UI for account quotas

--- app/controllers/pricing_controller.rb
+++ app/controllers/pricing_controller.rb
@@ -2,40 +2,32 @@ class PricingController < ApplicationController
   layout "admin"
   before_action :authenticate_user!
 
+  def usage
+    # Same as show but renders usage view
+    @account = current_user.account
+    @current_plan_label = "Current plan"
+
+    if @account
+      set_usage_data
+    end
+
+    if (pay_sub = @account&.active_subscription_record)
+      @subscription = pay_sub
+      @current_plan_label = "Current plan" if @subscription
+      @next_payment_date = calculate_next_payment_date(@subscription)
+      if @subscription
+        @trial_days_left = calculate_trial_days_left(@subscription)
+        @billing_period = format_billing_period(@subscription)
+      end
+    end
+  end
+
   def show
     @account = current_user.account
     @current_plan_label = "Current plan"
 
     if @account
-      # Event/Error tracking usage
-      @event_quota = @account.event_quota_value
-      @events_used = @account.events_used_in_billing_period
-      @events_remaining = [@event_quota - @events_used, 0].max
-
-      # AI Summaries usage
-      @ai_summaries_quota = @account.ai_summaries_quota
-      @ai_summaries_used = @account.ai_summaries_used_in_period
-      @ai_summaries_remaining = [@ai_summaries_quota - @ai_summaries_used, 0].max
-
-      # Pull Requests usage
-      @pull_requests_quota = @account.pull_requests_quota
-      @pull_requests_used = @account.pull_requests_used_in_period
-      @pull_requests_remaining = [@pull_requests_quota - @pull_requests_used, 0].max
-
-      # Uptime Monitors usage
-      @uptime_monitors_quota = @account.uptime_monitors_quota
-      @uptime_monitors_used = @account.uptime_monitors_used
-      @uptime_monitors_remaining = [@uptime_monitors_quota - @uptime_monitors_used, 0].max
-
-      # Status Pages usage
-      @status_pages_quota = @account.status_pages_quota
-      @status_pages_used = @account.status_pages_used
-      @status_pages_remaining = [@status_pages_quota - @status_pages_used, 0].max
-
-      # Projects usage
-      @projects_quota = @account.projects_quota
-      @projects_used = @account.projects_used
-      @projects_remaining = [@projects_quota - @projects_used, 0].max
+      set_usage_data
     end
 
     if (pay_sub = @account&.active_subscription_record)
@@ -50,6 +42,40 @@ class PricingController < ApplicationController
   end
 
   private
+
+  def set_usage_data
+    return unless @account
+
+    # Event/Error tracking usage
+    @event_quota = @account.event_quota_value
+    @events_used = @account.events_used_in_billing_period
+    @events_remaining = [@event_quota - @events_used, 0].max
+
+    # AI Summaries usage
+    @ai_summaries_quota = @account.ai_summaries_quota
+    @ai_summaries_used = @account.ai_summaries_used_in_period
+    @ai_summaries_remaining = [@ai_summaries_quota - @ai_summaries_used, 0].max
+
+    # Pull Requests usage
+    @pull_requests_quota = @account.pull_requests_quota
+    @pull_requests_used = @account.pull_requests_used_in_period
+    @pull_requests_remaining = [@pull_requests_quota - @pull_requests_used, 0].max
+
+    # Uptime Monitors usage
+    @uptime_monitors_quota = @account.uptime_monitors_quota
+    @uptime_monitors_used = @account.uptime_monitors_used
+    @uptime_monitors_remaining = [@uptime_monitors_quota - @uptime_monitors_used, 0].max
+
+    # Status Pages usage
+    @status_pages_quota = @account.status_pages_quota
+    @status_pages_used = @account.status_pages_used
+    @status_pages_remaining = [@status_pages_quota - @status_pages_used, 0].max
+
+    # Projects usage
+    @projects_quota = @account.projects_quota
+    @projects_used = @account.projects_used
+    @projects_remaining = [@projects_quota - @projects_used, 0].max
+  end
 
   def calculate_next_payment_date(subscription)
     return nil unless subscription&.current_period_end
