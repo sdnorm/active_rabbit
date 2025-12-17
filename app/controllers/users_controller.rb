29915ffbac4c4dec3b241494ab1a7aefb3e81d3f
vmorgunov <vmorgunov10@gmail.com>
2025-12-18 01:11:04 +0200

    fix test and brakeman

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :role)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
   def current_account
