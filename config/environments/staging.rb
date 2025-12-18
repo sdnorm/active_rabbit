59e2487a2e635d55c9dc25be2127e391a3258beb
vmorgunov <vmorgunov10@gmail.com>
2025-12-18 23:47:12 +0200

    update staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -8,4 +8,8 @@ Rails.application.configure do
   config.force_ssl = false
 
   config.log_level = :debug
+  config.active_record.attributes_for_inspect = [:id]
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  config.log_tags = [:request_id]
+  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
 end
