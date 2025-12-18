c8b226a34c415e540d183da3945972f809e726dc
vmorgunov <vmorgunov10@gmail.com>
2025-12-18 03:22:50 +0200

    Add config for deploy AR to staging

---
+++ config/environments/staging.rb
@@ -0,0 +1,10 @@
+Rails.application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+
+  config.consider_all_requests_local = true
+
+  config.force_ssl = false
+
+  config.log_level = :info
+end
