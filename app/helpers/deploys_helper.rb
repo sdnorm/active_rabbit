d7dad98fb3c2f47442e1736fb9bb7354ba3c12d8
vmorgunov <vmorgunov10@gmail.com>
2025-12-24 23:47:01 +0200

    update UI, add helper

--- app/helpers/deploys_helper.rb
+++ app/helpers/deploys_helper.rb
@@ -1,2 +1,20 @@
 module DeploysHelper
+  def live_for_human(deploy)
+    seconds = deploy.live_for_seconds.to_i
+    return "just now" if seconds < 60
+
+    minutes = seconds / 60
+    return "#{minutes}m" if minutes < 60
+
+    hours = minutes / 60
+    return "#{hours}h" if hours < 24
+
+    days = hours / 24
+    return "#{days}d" if days < 7
+
+    weeks = days / 7
+    remaining_days = days % 7
+
+    remaining_days.zero? ? "#{weeks}w" : "#{weeks}w #{remaining_days}d"
+  end
 end
--- app/models/deploy.rb
+++ app/models/deploy.rb
@@ -36,27 +36,4 @@ class Deploy < ApplicationRecord
   def performance_summary
     release.regression_summary
   end
-
-  def live_for_human
-    seconds = (Time.current - started_at).to_i
-    return "just now" if seconds < 60
-
-    minutes = seconds / 60
-    return "#{minutes}m" if minutes < 60
-
-    hours = minutes / 60
-    return "#{hours}h" if hours < 24
-
-    days = hours / 24
-    return "#{days}d" if days < 7
-
-    weeks = days / 7
-    remaining_days = days % 7
-
-    if remaining_days.zero?
-      "#{weeks}w"
-    else
-      "#{weeks}w #{remaining_days}d"
-    end
-  end
 end
