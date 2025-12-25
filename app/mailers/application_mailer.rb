0804aa962ec4bf54a2afeeb8cb0eabcb45511172
vmorgunov <vmorgunov10@gmail.com>
2025-12-25 17:32:00 +0200

    update gem and application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "ActiveRabbit <#{ENV.fetch("ALERT_FROM_EMAIL")}>"
+  default from: "ActiveRabbit <#{ENV.fetch("ALERT_FROM_EMAIL", "activerabbit@updates.activerabbit.ai")}>"
   layout "mailer"
 end
