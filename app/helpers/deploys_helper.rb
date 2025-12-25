1c1816452ca723e2cbf51d7521af6f8c74fe7d2d
vmorgunov <vmorgunov10@gmail.com>
2025-12-25 02:06:14 +0200

    Fix live for

--- app/helpers/deploys_helper.rb
+++ app/helpers/deploys_helper.rb
@@ -7,14 +7,15 @@ module DeploysHelper
     return "#{minutes}m" if minutes < 60
 
     hours = minutes / 60
-    return "#{hours}h" if hours < 24
+    remaining_minutes = minutes % 60
+    return "#{hours}h #{remaining_minutes}m" if hours < 24
 
     days = hours / 24
-    return "#{days}d" if days < 7
+    remaining_hours = hours % 24
+    return "#{days}d #{remaining_hours}h" if days < 7
 
     weeks = days / 7
     remaining_days = days % 7
-
     remaining_days.zero? ? "#{weeks}w" : "#{weeks}w #{remaining_days}d"
   end
 end
